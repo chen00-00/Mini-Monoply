a67d681b5e4effae197fce9763b14c2f6265d4ec
minhao <tony920201@gmail.com>
2025-06-12 16:10:05 +0800

    delete map

--- monopoly.h
+++ monopoly.h
@@ -50,46 +50,4 @@ class worldPlayer {
 
 };
 
-class MapUnit {
-public:
-    MapUnit(int id = -1, const std::string& name = "unknown") : id_(id), name_(name) {}
-    virtual ~MapUnit() = default;
-
-    int getId() const { return id_; }
-    std::string getName() const { return name_; }
-    int getStayPlayer() const { return who_is_here_; }
-    virtual void onPlayerEnter(Player& p) = 0;
-
-protected:
-    int id_;
-    std::string name_;
-    int who_is_here_ = -1;
-};
-
-class CollectableUnit : public MapUnit {
-public:
-    CollectableUnit(int id = -1, const std::string &name = "unknown", int price = 0, int fine = 0):MapUnit(id, name), price_(price), fine_(fine) {}
-    ~CollectableUnit() = default;
-    int getPrice() const  { return price_; }
-    int getFine() const  { return fine_ * num_cunits_; }
-    int getHost() const { return host_; }
-    int getCUM() const { return num_cunits_; }
-    void updateHost(const Player &p) { host_ = p.getId(); num_cunits_ = p.getCUM(); }
-    void reset() { host_ = -1; num_cunits_ = 0; }
-private:
-    int price_ = 0;
-    int fine_ = 0;
-    int host_ = -1;         // default: -1(nobody)
-    int num_cunits_ = 0;    // number of the CollectableUnit that the host has
-};
-
-class JailUnit : public MapUnit {
-public:
-    JailUnit(int id = -1, const std::string &name = "Jail"):MapUnit(id, name) {}
-    ~JailUnit() = default;
-    void onPlayerEnter(Player& p) override {
-        p.updateStatus(Status::InJail);
-    }
-};
-
 #endif
